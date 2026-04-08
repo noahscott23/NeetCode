786678bd5c4cb9f3f318e2b1f35fb2089a7cc570
Noah Scott <129787316+noahscott23@users.noreply.github.com>
2026-04-07 23:17:39 -0700

    Update: is-anagram - submission-15

--- Data Structures & Algorithms/is-anagram/submission-15.py
+++ Data Structures & Algorithms/is-anagram/submission-15.py
@@ -2,4 +2,22 @@ from collections import Counter
 
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        return Counter(s) == Counter(t)
+        if len(s) != len(t):
+            return False
+        
+        dict1 = {}
+        for i in s:
+            if i in dict1:
+                dict1[i] += 1
+            else:
+                dict1[i] = 1
+        
+        dict2 = {}
+        for i in t:
+            if i in dict2:
+                dict2[i] += 1
+            else:
+                dict2[i] = 1
+
+        return dict1 == dict2
+        
