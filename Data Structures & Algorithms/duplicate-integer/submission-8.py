44b76f813c1dfa68391b63d39c5e2cbf608e1007
Noah Scott <129787316+noahscott23@users.noreply.github.com>
2026-05-04 23:01:05 -0700

    Add: duplicate-integer - submission-8

---
+++ Data Structures & Algorithms/duplicate-integer/submission-8.py
@@ -0,0 +1,17 @@
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        hashMap = set() 
+
+        for i in nums:
+            if i in hashMap:
+                return True
+            else: 
+                hashMap.add(i)
+
+        return False
+
+        
+
+        
+
+        
